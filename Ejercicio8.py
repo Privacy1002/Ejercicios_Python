74f939f50d19ca98459daf6019058a743f629ded
Santiago Parra <124490200+Privacy1002@users.noreply.github.com>
2024-08-05 00:10:27 -0500

    Delete Ejercicio8.py

--- Ejercicio8.py
+++
@@ -1,4 +0,0 @@
-grados = input('Ingrese el valor a convertir de Grados Farenheit a Grados Celcius: ')
-result1 = float(grados) - int(32) 
-result2 = float(result1) / float(1.8)
-print("El valor convertido a Grados Celcius fue: " + str(result2) + " °C ")
