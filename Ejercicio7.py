53500b85b02a9ca1cd7f9b600ba0fc86531e015a
Santiago Parra <124490200+Privacy1002@users.noreply.github.com>
2024-08-05 00:10:16 -0500

    Delete Ejercicio7.py

--- Ejercicio7.py
+++
@@ -1,4 +0,0 @@
-radio = input('Ingrese la medida del Radio del Círculo: ')
-area1 = float(radio) * float(radio)
-area2 = float(3.14) * (area1)
-print("El área del Triángulo es: " + str(area2) + " cm² ")
