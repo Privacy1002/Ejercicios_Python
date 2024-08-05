dee6c7a67cad88b7c9c66bd5c1bedd65992bec9b
Santiago Parra <124490200+Privacy1002@users.noreply.github.com>
2024-08-05 00:10:06 -0500

    Delete Ejercicio6.py

--- Ejercicio6.py
+++
@@ -1,4 +0,0 @@
-base = input('Ingrese la medida de la Base: ')
-altura = input('Ingrese la medida de la Altura: ')
-area = float(base) * float(altura) / 2
-print("El área del Triángulo es: " + str(area) + " cm² ")
